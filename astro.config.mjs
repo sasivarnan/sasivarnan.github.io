dd33ea828047c10a5668242e0ca031c0252cb4de
Sasivarnan R <sasivarnan@hotmail.com>
2024-01-07 14:59:58 +0530

    Added integrations - react, tailwind, sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sasivarnan.github.io",
+  integrations: [tailwind(), sitemap(), react()]
+});
